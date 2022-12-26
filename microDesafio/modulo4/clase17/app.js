d80569de1e58075808d52272c9bacb23491c5d2b
johann-jerman <johannjerman014@gmail.com>
2022-12-26 14:29:24 -0300

    add style mercado liebre

--- microDesafio/modulo4/clase17/app.js
+++ microDesafio/modulo4/clase17/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = 3030;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
-})
+});
 
-app.listen(3000)
+app.listen(PORT);
